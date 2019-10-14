a355e84f7d6792e8cd7a7f5f8486c482391678f7
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2019-10-14 15:18:13 +0100

    created a controller for the user with full CRUD

--- src/main/java/com/qa/Controllers/UserController.java
+++ src/main/java/com/qa/Controllers/UserController.java
@@ -1,4 +1,45 @@
 package com.qa.Controllers;
 
+import com.qa.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.qa.Models.User;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.FetchType;
+
+@CrossOrigin
+@RestController
 public class UserController {
+
+    @Autowired
+    @ElementCollection(fetch = FetchType.LAZY)
+    private UserRepository repository;
+
+    @RequestMapping(value = "get/user/{userid}", method = RequestMethod.GET)
+    public User getUserById(@PathVariable String userid){
+        return repository.findOne(userid);
+    }
+
+    @RequestMapping(value = "delete/user/{userid}", method = RequestMethod.DELETE)
+    public User deleteUserById(@PathVariable String userid){
+        User user = repository.findOne(userid);
+        repository.delete(user);
+        return user;
+    }
+
+    @RequestMapping(value = "add/user/", method = RequestMethod.POST)
+    public User addUserById(@RequestBody User user){
+        return repository.saveAndFlush(user);
+    }
+
+    @RequestMapping(value = "update/user/{userid", method = RequestMethod.PUT)
+    public User updateUserById(@PathVariable String userid, @RequestBody User user){
+        User currentUser = repository.findOne(userid);
+        currentUser.setPassword(user.getPassword());
+        repository.saveAndFlush(currentUser);
+        return currentUser;
+    }
+
+
 }
