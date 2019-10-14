8ff0d1f16296eec1285232ffb26fdbc8ec5a314e
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2019-10-14 14:37:35 +0100

    created a user model

--- src/main/java/com/qa/Models/User.java
+++ src/main/java/com/qa/Models/User.java
@@ -1,4 +1,39 @@
 package com.qa.Models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    private String userid;
+
+    private String password;
+
+    public User(){
+
+    }
+
+    public User(String userid, String password){
+        this.userid = userid;
+        this.password = password;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
