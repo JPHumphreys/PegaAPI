6b13749179241917a4b9f5d10359e2eef7303052
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2019-10-14 15:48:55 +0100

    adding comments and filters in board game repo

--- src/main/java/com/qa/Controllers/BoardGameController.java
+++ src/main/java/com/qa/Controllers/BoardGameController.java
@@ -1,4 +1,24 @@
 package com.qa.Controllers;
 
+
+import com.qa.Repositories.BoardGameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.qa.Models.BoardGame;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.FetchType;
+
+@CrossOrigin
+@RestController
+
 public class BoardGameController {
+
+    @Autowired
+    @ElementCollection(fetch = FetchType.LAZY)
+    private BoardGameRepository repository;
+
+
+
+
 }
--- src/main/java/com/qa/Repositories/BoardGameRepository.java
+++ src/main/java/com/qa/Repositories/BoardGameRepository.java
@@ -3,6 +3,17 @@ package com.qa.Repositories;
 import com.qa.Models.BoardGame;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardGameRepository extends JpaRepository<BoardGame, String> {
+
+    //returns all that has a player min equal or less than the value.
+    List<BoardGame> findByPlayerMinLessThanEqual(int playerNum);
+    //returns all that has a player max equal or greater than the value.
+    List<BoardGame> findByPlayerMaxGreaterThanEqual(int playerNum);
+    //returns all by price less than or equal to the value.
+    List<BoardGame> findAllByPriceLessThanEqual(float price);
+    //returns all by time less than or equal to the value.
+    List<BoardGame> findAllByTimeLessThanEqual(int time);
 
 }
