bc4143f12addf6c1ce1d6a6c6d3d72b042db9094
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2019-10-14 15:07:05 +0100

    created a boardgame model

--- src/main/java/com/qa/Models/BoardGame.java
+++ src/main/java/com/qa/Models/BoardGame.java
@@ -1,4 +1,96 @@
 package com.qa.Models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "boardgame")
+
 public class BoardGame {
+
+    @Id
+    private String name;
+
+    private int playerMin;
+    private int playerMax;
+    private int time;
+    private int age;
+    private String description;
+    private String imgLocation;
+
+    private float price;
+    private int stock;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPlayerMin() {
+        return playerMin;
+    }
+
+    public void setPlayerMin(int playerMin) {
+        this.playerMin = playerMin;
+    }
+
+    public int getPlayerMax() {
+        return playerMax;
+    }
+
+    public void setPlayerMax(int playerMax) {
+        this.playerMax = playerMax;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImgLocation() {
+        return imgLocation;
+    }
+
+    public void setImgLocation(String imgLocation) {
+        this.imgLocation = imgLocation;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
 }
